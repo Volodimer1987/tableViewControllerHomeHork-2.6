75dea4a76952a02d98cc4e54d49499ee04376e90
Vladimir <kynicin87@mail.ru>
2020-08-26 13:17:40 +0300

    Initial Commit

---
+++ tableViewControllerHomeHork 2.6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewControllerHomeHork 2.6
+//
+//  Created by vladimir gennadievich on 26.08.2020.
+//  Copyright © 2020 Vladimir Gennadievich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
